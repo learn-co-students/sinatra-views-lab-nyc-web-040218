8f632ada5c18d0ecfa68446c7f310dfe20245d8d
Ahamed <ahamedali95@gmail.com>
2018-04-25 09:47:19 -0400

    Done.

--- app.rb
+++ app.rb
@@ -4,5 +4,20 @@ class App < Sinatra::Base
 		erb :index
 	end
 
+	get "/hello" do
+		erb :hello
+	end
 
+	get "/goodbye" do
+		erb :goodbye
+	end
+
+	get "/date" do
+		@day_name = Time.now.strftime("%A")
+		@month = Time.now.strftime("%B")
+		@day = Time.now.strftime("%d")
+		@year = Time.now.strftime("%Y")
+
+		erb :date
+	end
 end
